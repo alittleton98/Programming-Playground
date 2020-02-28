e873889bbe15f8bdc77d792d40547207ba8f16ff
alittleton98 <andrew.littleton1998@gmail.com>
2020-02-27 20:23:08 -0600

    database lab work

--- SchoolDev/Database Programming C353/Labs-Littleton/02-Templates-Lab-Littleton/flaskDemo/flaskDemo.py
+++ SchoolDev/Database Programming C353/Labs-Littleton/02-Templates-Lab-Littleton/flaskDemo/flaskDemo.py
@@ -1,31 +1,37 @@
 from flask import Flask, render_template, url_for
+
 app = Flask(__name__)
 
 posts = [
     {
-        'author': 'Channah Naiman',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 20, 2018'
+        "author": "Channah Naiman",
+        "title": "Blog Post 1",
+        "content": "First post content",
+        "date_posted": "April 20, 2018",
     },
     {
-        'author': 'Peter Dordal',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 21, 2018'
-    }
+        "author": "Peter Dordal",
+        "title": "Blog Post 2",
+        "content": "Second post content",
+        "date_posted": "April 21, 2018",
+    },
 ]
+
+favSnacks = ["Lime Chips", "honey roasted peanuts", "toast"]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html', title='Hey', posts=posts)
+    return render_template("home.html", title="Hey", posts=posts)
 
 
 @app.route("/about")
 def about():
-    return render_template('about.html', title='About')
+    return render_template("about.html", title="About")
 
 
+@app.route("/lab")
+def lab():
+    return
 
